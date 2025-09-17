cd4d97173088fe11ca82c68ffda1980eb8c16341
HyunMin Lim <lhm6179@naver.com>
2025-09-17 14:35:58 +0900

    Insert & Contains & remove 추가

---
+++ 20250917/BST.cs
@@ -0,0 +1,117 @@
+﻿
+
+namespace BST_BinarySearchTree
+{
+    class BSTNode
+    {
+        public int Key;
+        public BSTNode Left;
+        public BSTNode Right;
+
+        public BSTNode(int key)
+        {
+            this.Key = key;
+        }
+    }
+
+    class BinarySearchTree
+    {
+        private BSTNode _root;
+        public void Insert(int key)
+        {
+            _root = InsertRec(_root, key);
+        }
+
+        private BSTNode InsertRec(BSTNode node, int key)
+        {
+            if (node == null)
+                return new BSTNode(key);
+
+            if (node.Key > key)
+                node.Left = InsertRec(node.Left, key);
+
+            else
+                node.Right = InsertRec(node.Right, key);
+
+            return node;
+        }
+
+        public bool Contains(int key)
+        {
+            BSTNode node = _root;
+
+            while (node != null)
+            {
+                if (node.Key == key) return true;
+                else
+                {
+                    if (node.Key > key) node = node.Left;
+                    else node = node.Right;
+                }
+            }
+
+            return false;
+        }
+
+        public void Remove(int key)
+        {
+            _root = RemoveRec(_root, key);
+        }
+
+        private BSTNode RemoveRec(BSTNode node, int key)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            else
+            {
+                if (node.Key > key) node.Left = RemoveRec(node.Left, key);
+                else if (node.Key < key) node.Right = RemoveRec(node.Right, key);
+                else
+                {
+                    if (node.Left != null && node.Right != null) 
+                    { 
+                        FindMin(node.Right).Left = node.Left;
+                        node = FindMin(node.Right); 
+                    }
+                    else if (node.Right != null)
+                        node = node.Right;
+
+                    else if (node.Left != null) 
+                        node = node.Left;
+
+                    else return null;
+                }
+            }
+
+            return node;
+        }
+
+        private BSTNode FindMin(BSTNode node)
+        {
+            if (node.Left != null) FindMin(node.Left);
+
+            return node;
+        }
+    }
+
+    class BST
+    {
+        static void Main()
+        {
+            var bsT = new BinarySearchTree();
+            int[] data = { 16, 14, 78, 31, 28 , 87, 90 };
+            foreach(int i in data)
+            {
+                bsT.Insert(i);
+            }
+
+            bsT.Remove(78);
+
+            Console.WriteLine( bsT.Contains(87) );
+
+        }
+    }
+}
--- 20250917/TreeNode.cs
+++ 20250917/TreeNode.cs
@@ -1,74 +1,74 @@
-﻿namespace _20250917
-{
-    class TreeNode<T>
-    {
-        public T Data;
-        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
-    }
-    class _TreeNode
-    {
-        static TreeNode<string> MakeTree()
-        {
-            TreeNode<string> root = new TreeNode<string>() { Data = "A" };
-            {
-                TreeNode<string> nodeB = new TreeNode<string>() { Data = "B" };
-                {
-                    TreeNode<string> nodeD = new TreeNode<string>() { Data = "D" };
-                    {
-                        TreeNode<string> nodeH = new TreeNode<string>() { Data = "H" };
-                        TreeNode<string> nodeI = new TreeNode<string>() { Data = "I" };
+﻿//namespace _20250917
+//{
+//    class TreeNode<T>
+//    {
+//        public T Data;
+//        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
+//    }
+//    class _TreeNode
+//    {
+//        static TreeNode<string> MakeTree()
+//        {
+//            TreeNode<string> root = new TreeNode<string>() { Data = "A" };
+//            {
+//                TreeNode<string> nodeB = new TreeNode<string>() { Data = "B" };
+//                {
+//                    TreeNode<string> nodeD = new TreeNode<string>() { Data = "D" };
+//                    {
+//                        TreeNode<string> nodeH = new TreeNode<string>() { Data = "H" };
+//                        TreeNode<string> nodeI = new TreeNode<string>() { Data = "I" };
 
-                        nodeD.Children.Add(nodeH);
-                        nodeD.Children.Add(nodeI);
-                    }
-                    TreeNode<string> nodeE = new TreeNode<string>() { Data = "E" };
+//                        nodeD.Children.Add(nodeH);
+//                        nodeD.Children.Add(nodeI);
+//                    }
+//                    TreeNode<string> nodeE = new TreeNode<string>() { Data = "E" };
 
-                    nodeB.Children.Add(nodeD);
-                    nodeB.Children.Add(nodeE);
-                }
-                TreeNode<string> nodeC = new TreeNode<string>() { Data = "C" };
-                {
-                    TreeNode<string> nodeF = new TreeNode<string>() { Data = "F" };
-                    TreeNode<string> nodeG = new TreeNode<string>() { Data = "G" };
+//                    nodeB.Children.Add(nodeD);
+//                    nodeB.Children.Add(nodeE);
+//                }
+//                TreeNode<string> nodeC = new TreeNode<string>() { Data = "C" };
+//                {
+//                    TreeNode<string> nodeF = new TreeNode<string>() { Data = "F" };
+//                    TreeNode<string> nodeG = new TreeNode<string>() { Data = "G" };
 
-                    nodeC.Children.Add(nodeF);
-                    nodeC.Children.Add(nodeG);
-                }
+//                    nodeC.Children.Add(nodeF);
+//                    nodeC.Children.Add(nodeG);
+//                }
 
-                root.Children.Add(nodeB);
-                root.Children.Add(nodeC);
-            }
+//                root.Children.Add(nodeB);
+//                root.Children.Add(nodeC);
+//            }
 
-            return root;
-        }
+//            return root;
+//        }
 
-        static void PrintTree(TreeNode<string> node)
-        {
-            Console.WriteLine(node.Data);
+//        static void PrintTree(TreeNode<string> node)
+//        {
+//            Console.WriteLine(node.Data);
 
-            for (int i = 0; i < node.Children.Count; i++)
-            {
-                PrintTree(node.Children[i]);
-            }
-        }
+//            for (int i = 0; i < node.Children.Count; i++)
+//            {
+//                PrintTree(node.Children[i]);
+//            }
+//        }
 
-        static int GetHeight(TreeNode<string> node)
-        {
-            int height = 0;
+//        static int GetHeight(TreeNode<string> node)
+//        {
+//            int height = 0;
 
-            for(int i = 0; i < node.Children.Count; i++)
-            {
-                int newHeight = GetHeight(node.Children[i]) + 1;
-                if ( newHeight > height ) height = newHeight;
-            }
+//            for(int i = 0; i < node.Children.Count; i++)
+//            {
+//                int newHeight = GetHeight(node.Children[i]) + 1;
+//                if ( newHeight > height ) height = newHeight;
+//            }
 
-            return height;
-        }
+//            return height;
+//        }
 
-        static void Main(string[] args)
-        {
-            var root = MakeTree();
-            Console.WriteLine( GetHeight(root)); 
-        }
-    }
-}
+//        static void Main(string[] args)
+//        {
+//            var root = MakeTree();
+//            Console.WriteLine( GetHeight(root)); 
+//        }
+//    }
+//}
